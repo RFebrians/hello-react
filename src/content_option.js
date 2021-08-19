0df16a2959338b5c7e557be9e9969cc0b17b8f87
R. Febriansyah <71978432+RFebrians@users.noreply.github.com>
2021-08-20 02:13:41 +0700

    update post

--- src/content_option.js
+++ src/content_option.js
@@ -1,161 +1,169 @@
-
 const logotext = "React Project";
 const meta = {
-    title: "React Project",
-    description: "I’m John Doe data scientist _ Full stack devloper,currently working in Berlin",
+  title: "React Project",
+  description:
+    "I’m John Doe data scientist _ Full stack devloper,currently working in Berlin"
 };
 
 const introdata = {
-    title: "Welcome to",
-    animated: {
-        first: "React JS",
-        second: "React Native",
-        third: "React Libraries",
-    },
-    description: "To get started , please click button below",
-    img_url: "https://wallpaperaccess.com/full/3949076.jpg",
+  title: "Welcome to",
+  animated: {
+    first: "React JS",
+    second: "React Native",
+    third: "React Libraries"
+  },
+  description: "To get started , please click button below",
+  img_url: "https://wallpaperaccess.com/full/3949076.jpg"
 };
 
 const dataabout = {
-    title: "React Native Handbook",
-    aboutme: "Created using React Native framework , Rendering Native View with Mobile Apps . ",
+  title: "React Native Handbook",
+  aboutme:
+    "Created using React Native framework , Rendering Native View with Mobile Apps . "
 };
-const worktimeline = [{
-        jobtitle: "test",
-        where: "test",
-        date: "2020",
-    },
-    {
-        jobtitle: "test",
-        where: "test",
-        date: "2019",
-    },
-    {
-        jobtitle: "test",
-        where: "test",
-        date: "2019",
-    },
+const worktimeline = [
+  {
+    jobtitle: "test",
+    where: "test",
+    date: "2020"
+  },
+  {
+    jobtitle: "test",
+    where: "test",
+    date: "2019"
+  },
+  {
+    jobtitle: "test",
+    where: "test",
+    date: "2019"
+  }
 ];
 
-const tech = [{
-        name: "React Native",
-        value: 98,
-    },
-    {
-        name: "Wordpress CMS",
-        value: 85,
-    },
-    {
-        name: "MySql",
-        value: 50,
-    },
-    {
-        name: "Apache Server",
-        value: 95,
-    },
+const tech = [
+  {
+    name: "React Native",
+    value: 98
+  },
+  {
+    name: "Wordpress CMS",
+    value: 85
+  },
+  {
+    name: "MySql",
+    value: 50
+  },
+  {
+    name: "Apache Server",
+    value: 95
+  }
 ];
 
-const services = [{
-        title: "Target Rendering URL",
-        description: "https://research.forwardproject.xyz/",
-    },
-    {
-        title: "GitHub",
-        description: "https://github.com/RFebrians/React-Native-Handbook",
-    },
-    {
-        title: "Live Demo , Expo Snack ",
-        description: "https://snack.expo.dev/@zegveld/delicious-pretzels",
-    },
+const services = [
+  {
+    title: "Target Rendering URL",
+    description: "https://research.forwardproject.xyz/"
+  },
+  {
+    title: "GitHub",
+    description: "https://github.com/RFebrians/React-Native-Handbook"
+  },
+  {
+    title: "Live Demo , Expo Snack ",
+    description: "https://snack.expo.dev/@zegveld/delicious-pretzels"
+  }
 ];
 
-const dataportfolio = [{
-        img: "https://miro.medium.com/max/1400/1*CeuWv9fCjD1uTiTuKytnBQ.png",
-        desctiption: "Introducing React ",
-        link: "../post2",
-    },
-    {
-        img: "https://images.unsplash.com/photo-1581276879432-15e50529f34b?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1500&q=80",
-        desctiption: "Fundamental",
-        link: "../post2",
-    },
-    {
-        img: "https://miro.medium.com/max/2000/1*-Ijet6kVJqGgul6adezDLQ.png",
-        desctiption: "Hooks",
-        link: "../post3",
-    },
-    {
-        img: "https://images.unsplash.com/photo-1526498460520-4c246339dccb?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=750&q=80",
-        desctiption: "Advanced Hooks ",
-        link: "../post3",
-    },
-    {
-        img: "https://miro.medium.com/max/1400/1*CeuWv9fCjD1uTiTuKytnBQ.png",
-        desctiption: "Create React App",
-        link: "../post3",
-    },
-    {
-        img: "https://miro.medium.com/max/1400/1*CeuWv9fCjD1uTiTuKytnBQ.png",
-        desctiption: "React Performance",
-        link: "#",
-    },
+const dataportfolio = [
+  {
+    img: "https://miro.medium.com/max/1400/1*CeuWv9fCjD1uTiTuKytnBQ.png",
+    desctiption: "1. Introducing React ",
+    link: "../post2"
+  },
+  {
+    img:
+      "https://images.unsplash.com/photo-1581276879432-15e50529f34b?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1500&q=80",
+    desctiption: "2. Fundamental",
+    link: "../post3"
+  },
+  {
+    img: "https://miro.medium.com/max/2000/1*-Ijet6kVJqGgul6adezDLQ.png",
+    desctiption: "Hooks",
+    link: "../post3"
+  },
+  {
+    img:
+      "https://images.unsplash.com/photo-1526498460520-4c246339dccb?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=750&q=80",
+    desctiption: "Advanced Hooks ",
+    link: "../post3"
+  },
+  {
+    img: "https://miro.medium.com/max/1400/1*CeuWv9fCjD1uTiTuKytnBQ.png",
+    desctiption: "Create React App",
+    link: "../post3"
+  },
+  {
+    img: "https://miro.medium.com/max/1400/1*CeuWv9fCjD1uTiTuKytnBQ.png",
+    desctiption: "React Performance",
+    link: "#"
+  },
 
-    {
-        img: "https://miro.medium.com/max/1400/1*CeuWv9fCjD1uTiTuKytnBQ.png",
-        desctiption: "Testing React Apps",
-        link: "#",
-    },
-    {
-        img: "https://miro.medium.com/max/1400/1*CeuWv9fCjD1uTiTuKytnBQ.png",
-        desctiption: "React Suspense",
-        link: "#",
-    },
-    {
-        img: "https://miro.medium.com/max/1400/1*CeuWv9fCjD1uTiTuKytnBQ.png",
-        desctiption: "Build an Epic React Apps",
-        link: "#",
-    },
-    {
-        img: "https://miro.medium.com/max/1400/1*CeuWv9fCjD1uTiTuKytnBQ.png",
-        desctiption: "React-Native Handbook",
-        link: "#",
-    },
-    {
-        img: "https://miro.medium.com/max/1400/1*CeuWv9fCjD1uTiTuKytnBQ.png",
-        desctiption: "Glosarium",
-        link: "#",
-    },
-    {
-        img: "https://miro.medium.com/max/1400/1*CeuWv9fCjD1uTiTuKytnBQ.png",
-        desctiption: "Credits",
-        link: "#",
-    },
+  {
+    img: "https://miro.medium.com/max/1400/1*CeuWv9fCjD1uTiTuKytnBQ.png",
+    desctiption: "Testing React Apps",
+    link: "#"
+  },
+  {
+    img: "https://miro.medium.com/max/1400/1*CeuWv9fCjD1uTiTuKytnBQ.png",
+    desctiption: "React Suspense",
+    link: "#"
+  },
+  {
+    img: "https://miro.medium.com/max/1400/1*CeuWv9fCjD1uTiTuKytnBQ.png",
+    desctiption: "Build an Epic React Apps",
+    link: "#"
+  },
+  {
+    img: "https://miro.medium.com/max/1400/1*CeuWv9fCjD1uTiTuKytnBQ.png",
+    desctiption: "React-Native Handbook",
+    link: "#"
+  },
+  {
+    img: "https://miro.medium.com/max/1400/1*CeuWv9fCjD1uTiTuKytnBQ.png",
+    desctiption: "Glosarium",
+    link: "#"
+  },
+  {
+    img: "https://miro.medium.com/max/1400/1*CeuWv9fCjD1uTiTuKytnBQ.png",
+    desctiption: "Credits",
+    link: "#"
+  }
 ];
 
 const contactConfig = {
-    YOUR_EMAIL: "name@domain.com",
-    YOUR_FONE: "(555)123-4567",
-    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi vehicula eu nunc et sollicitudin. Cras pulvinar, nisi at imperdiet pharetra. ",
-    YOUR_SERVICE_ID: "service_id",
-    YOUR_TEMPLATE_ID: "template_id",
-    YOUR_USER_ID: "user_id",
+  YOUR_EMAIL: "name@domain.com",
+  YOUR_FONE: "(555)123-4567",
+  description:
+    "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi vehicula eu nunc et sollicitudin. Cras pulvinar, nisi at imperdiet pharetra. ",
+  YOUR_SERVICE_ID: "service_id",
+  YOUR_TEMPLATE_ID: "template_id",
+  YOUR_USER_ID: "user_id"
 };
 
 const socialprofils = {
-    github: "https://github.com",
-    facebook: "https://facebook.com",
-    linkedin: "https://linkedin.com",
-    twitter: "https://twitter.com",
+  github: "https://github.com",
+  facebook: "https://facebook.com",
+  linkedin: "https://linkedin.com",
+  twitter: "https://twitter.com"
 };
 export {
-    meta,
-    dataabout,
-    dataportfolio,
-    worktimeline,
-    tech,
-    services,
-    introdata,
-    contactConfig,
-    socialprofils,
-    logotext,
+  meta,
+  dataabout,
+  dataportfolio,
+  worktimeline,
+  tech,
+  services,
+  introdata,
+  contactConfig,
+  socialprofils,
+  logotext
 };
--- src/pages/post3/index.js
+++ src/pages/post3/index.js
@@ -1,6 +1,7 @@
 import React from "react";
 import "./style.css";
 import MDEditor from "@uiw/react-md-editor";
+import ReactEmbedGist from "react-embed-gist";
 import { Helmet, HelmetProvider } from "react-helmet-async";
 import { Container, Row, Col } from "react-bootstrap";
 import {
@@ -8,51 +9,61 @@ import {
   meta,
   worktimeline,
   skills,
-  services,
+  services
 } from "../../content_option";
 
 export default function Post3() {
   return (
     <HelmetProvider>
-        <Helmet>
-          <meta charSet="utf-8" />
-          <title> About | {meta.title}</title>
-          <meta
-            name="description"
-            content={meta.description}
+      <Helmet>
+        <meta charSet="utf-8" />
+        <title> Styling | {meta.title}</title>
+        <meta name="description" content={meta.description} />
+      </Helmet>
+      <Row className="mb-5 mt-3">
+        <Col lg="5">
+          <h1 className="display-4 mb-4"> Styling in React </h1>{" "}
+          <hr className="t_border my-4 ml-0 text-left" />
+        </Col>
+      </Row>
+      <div className="ab_block">
+        <br></br>
+        <h4>
+          Pada React kita dapat menggunakan beberapa Styling untuk Components ,
+          diantaranya adalah CSS , SCSS atau Styled Components dari Libraries
+          untuk React .
+        </h4>
+        <br></br>
+        <h2>Metode 1 CSS</h2>
+        <div>
+          <ReactEmbedGist
+            gist="RFebrians/41d0155c41cb222ec00ed668558ab7d1"
+            file="MyButton.js"
           />
-        </Helmet>
-        <Row className="mb-5 mt-3">
-          <Col lg="5">
-            <h1 className="display-4 mb-4"> Create React App </h1>{" "}
-            <hr className="t_border my-4 ml-0 text-left" />
-          </Col>
-        </Row>
-        <div className="ab_block">
-        <h2> Initializing </h2>
-        <br></br>
-        <h4>Sebelum memulai React Application pastikan anda telah menginstall node JS dan npm https://nodejs.org  . </h4>
-        <br></br>
-        <p>Pilihlah salah satu IDE atau Text Editor yang cocok untuk digunakan .</p>
-<p>Dan untuk memulai anda akan sering menggunakan command atau terminal sebagai perantara</p>
-<p>Buka Command dan cek version node dan npm , </p>
-<p>alternatifnya anda dapat menggunakan yarn</p>
-        <MDEditor.Markdown source="`$ node –version`" />
-        <MDEditor.Markdown source="`$ npm –version`" />
         </div>
+        <ReactEmbedGist
+          gist="RFebrians/41d0155c41cb222ec00ed668558ab7d1"
+          file="styles.css"
+        />
+        <p>Dari kesimpulan code diatas </p>
+        <li> CSS </li>
+      </div>
 
-        <div className="ab_block">
-        <h2>Create React App</h2>
+      <div className="ab_block">
+        <h2>Metode 2 Styled Component</h2>
         <p> Jalankan Command berikut pada Command Line atau terminal </p>
-        <MDEditor.Markdown source="`$ npx create-react-app my-app`" />
-        <MDEditor.Markdown source="`$ cd my-app`" />
-        <MDEditor.Markdown source="`$ npm start`" />
+        <ReactEmbedGist
+          gist="RFebrians/41d0155c41cb222ec00ed668558ab7d1"
+          file="styles.css"
+        />
         <br></br>
-        <h4> Browser akan mengalihkan ke locallhost:3000 artinya React-App siap dibuat .
-        Tetapi sebelum memulai mari kita pelajari fundamentalnya secara singkat . </h4>
-
-        </div>
+        <h4>
+          {" "}
+          Browser akan mengalihkan ke locallhost:3000 artinya React-App siap
+          dibuat . Tetapi sebelum memulai mari kita pelajari fundamentalnya
+          secara singkat .{" "}
+        </h4>
+      </div>
     </HelmetProvider>
-
   );
 }
