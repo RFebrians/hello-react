656047a7c89b2300ed8008131f8a46f96b7fe4fc
R. Febriansyah <71978432+RFebrians@users.noreply.github.com>
2021-12-13 13:14:57 +0700

    fix something mistake please consider it before using a pull request , i'm a newbie too ^_^

--- src/app/routes.js
+++ src/app/routes.js
@@ -1,15 +1,10 @@
-
 import React from "react";
 import { Route } from "react-router-dom";
 import { AnimatedSwitch } from "react-router-transition";
 import Home from "../pages/home";
 import Post from "../pages/post";
 import Post2 from "../pages/post2";
-import Post3 from "../pages/post3";
 import Post4 from "../pages/post4";
-import Post5 from "../pages/post5";
-import Post6 from "../pages/post6";
-import Post7 from "../pages/post7";
 import Index from "../pages/index";
 import Demo from "../pages/demo";
 
@@ -25,11 +20,7 @@ function AppRoutes() {
       <Route exact path="/" component={Home} />
       <Route path="/post" component={Post} />
       <Route path="/post2" component={Post2} />
-      <Route path="/post3" component={Post3} />
       <Route path="/post4" component={Post4} />
-      <Route path="/post5" component={Post5} />
-      <Route path="/post6" component={Post6} />
-      <Route path="/post7" component={Post7} />
       <Route path="/index" component={Index} />
       <Route path="/demo" component={Demo} />
       <Route path="*" component={Home} />
@@ -40,20 +31,20 @@ function AppRoutes() {
 function mapStyles(styles) {
   return {
     transition: `transform 100ms ease`,
-    transform: `translateY(${styles.translateY}%)`,
+    transform: `translateY(${styles.translateY}%)`
   };
 }
 
 const anim = {
   atEnter: {
-    translateY: 100,
+    translateY: 100
   },
   atLeave: {
-    translateY: -130,
+    translateY: -130
   },
   atActive: {
-    translateY: 0,
-  },
+    translateY: 0
+  }
 };
 
 export default AppRoutes;
